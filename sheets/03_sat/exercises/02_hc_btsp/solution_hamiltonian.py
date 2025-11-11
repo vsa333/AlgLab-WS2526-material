fe0b24af4be588618e2f5c9ad756d7e21d128b5c
Valentin Sarezki <v.sarezki@tu-braunschweig.de>
2025-11-11 14:38:32 +0100

    formulation for decision variant

--- sheets/03_sat/exercises/02_hc_btsp/solution_hamiltonian.py
+++ sheets/03_sat/exercises/02_hc_btsp/solution_hamiltonian.py
@@ -9,8 +9,16 @@ class HamiltonianCycleModel:
         self.graph = graph
         self.solver = SATSolver("Minicard")
         self.assumptions = []
+        self.edges = self.graph.edges()
         # TODO: Implement me!
+        self.vars = {edge: i for i, edge in enumerate(self.edges)}
 
+        self.add_degree_constraints()
+
+    def add_degree_constraints(self):
+        for node in self.graph.nodes():
+            self.solver.add_atmost([self.vars[(node, neighbor)] for neighbor in self.graph.neighbors(node)], 2)
+            self.solver.add_atmost([-self.vars[(node, neighbor)] for neighbor in self.graph.neighbors(node)], 2)
 
     def solve(self) -> list[tuple[int, int]] | None:
         """
