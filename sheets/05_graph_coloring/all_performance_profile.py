fec7e08931de54678fe3e4f6bd79aad2c00ef22a
Valentin Sarezki <v.sarezki@tu-braunschweig.de>
2025-12-04 21:30:42 +0100

    added more benchmarks

--- sheets/05_graph_coloring/all_performance_profile.py
+++ sheets/05_graph_coloring/all_performance_profile.py
@@ -7,6 +7,7 @@ from models.cp_sat.rep_cp import REP_CP
 from models.cp_sat.cp_alldiff import CP_ALLDIFF
 from models.cp_sat.cp_neq import CP_NEQ
 from models.sat.sat_form_solver import GCSATSolver
+from models.preprocessor import GCPreprocessor
 
 import os
 from _gclib import GCGraphInstance
@@ -27,33 +28,34 @@ data = pd.DataFrame({
 
 GRAPH_TYPE = "barabasi_albert"
 
-instances = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9]
+instances = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9]
 
-strategies = ["ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF", "GCSATSolver", "ASSCP", "ASS_SCP", "REP_CP", "CP_NEQ", "CP_ALLDIFF"]
 
-metrics = [11.0, 10.0, 11.0, 10.0, 10.0, 11.0, 11.0, 10.0, 11.0, 10.0, 10.0, 11.0, 12.0, 10.0, 11.0, 10.0, 10.0, 11.0, 11.0, 10.0, 10.0, 10.0, 10.0, 11.0, 10.0, 10.0, 11.0, 11.0, 10.0, 11.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 13, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0, 10, 10.0, 10.0, 10.0, 10.0, 10.0]
+strategies = ["ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB", "ASS_GRB", "ASS_S_GRB", "REP_GRB"]
+
+metrics = [11.0, 10.0, 11.0, 10.0, 10.0, 11.0, 11.0, 10.0, 11.0, 10.0, 10.0, 11.0, 12.0, 10.0, 11.0, 10.0, 10.0, 11.0, 10.0, 10.0, 10.0, 10.0, 10.0, 11.0, 10.0, 10.0, 11.0, 10.0, 10.0, 11.0]
 
 
 gc = GCGraphInstance()
 
 for i in range(10):
-    graph = gc.graphs[f"{GRAPH_TYPE}_graph{i}"]
+    graph_ = gc.graphs[f"{GRAPH_TYPE}_graph{i}"]
     """ 
     with open(f"instances/erdos_renyi_graph{name}.col", "w", encoding="utf-8") as file:
         for u, v in graph.edges():
             file.write(f"e {u} {v}\n")
-    
+    """
+
+    pp = GCPreprocessor(graph_)
+    graph = pp.preprocess()
+
     sat = GCSATSolver(graph)
     sol_sat = sat.solve(60)
     instances.append(i)
     strategies.append("GCSATSolver")
     metrics.append(sol_sat)
 
-    instances_lb.append(i)
-    strategies_lb.append("GCSATSolver")
-    metrics_lb.append(sat.lb)
-
-
+    """
     ng = ASSGRB(graph)
     sol_ng = ng.solve(60)
     instances.append(name)
@@ -73,6 +75,7 @@ for i in range(10):
     instances.append(name)
     strategies.append("REP")
     metrics.append(sol_ds)
+    """
 
     ng = ASSCP(graph)
     sol_ng = ng.solve(60)
@@ -80,22 +83,12 @@ for i in range(10):
     strategies.append("ASSCP")
     metrics.append(sol_ng)
     
-    instances_lb.append(i)
-    strategies_lb.append("ASSCP")
-    metrics_lb.append(ng.lb)
-
-
 
     ng = ASS_SCP(graph)
     sol_ng = ng.solve(60)
     instances.append(i)
     strategies.append("ASS_SCP")
     metrics.append(sol_ng)
-
-    instances_lb.append(i)
-    strategies_lb.append("ASS_SCP")
-    metrics_lb.append(ng.lb)
-
 
 
     ng = REP_CP(graph)
@@ -104,22 +97,12 @@ for i in range(10):
     strategies.append("REP_CP")
     metrics.append(sol_ng)
 
-    instances_lb.append(i)
-    strategies_lb.append("REP_CP")
-    metrics_lb.append(ng.lb)
-
-
 
     ng = CP_NEQ(graph)
     sol_ng = ng.solve(60)
     instances.append(i)
     strategies.append("CP_NEQ")
     metrics.append(sol_ng)
-
-    instances_lb.append(i)
-    strategies_lb.append("CP_NEQ")
-    metrics_lb.append(ng.lb)
-
 
 
     ng = CP_ALLDIFF(graph)
@@ -128,18 +111,11 @@ for i in range(10):
     strategies.append("CP_ALLDIFF")
     metrics.append(sol_ng) 
 
-    instances_lb.append(i)
-    strategies_lb.append("CP_ALLDIFF")
-    metrics_lb.append(ng.lb)
 
-    """
-
-"""
-with open(f"all_dataset_{GRAPH_TYPE}_lb.txt", "w", encoding="utf-8") as file:
+with open(f"all_dataset_{GRAPH_TYPE}_obj_preprocessed.txt", "w", encoding="utf-8") as file:
     file.write(f"[{', '.join(map(str, instances))}]\n")
     file.write(f'''["{'", "'.join(strategies)}"]\n''')
     file.write(f"[{', '.join(map(str, metrics))}]\n")
-"""
 
 
 data = pd.DataFrame({
@@ -154,13 +130,13 @@ ax = ppp.plot_performance_profile(
     instance_column="instance",
     strategy_column="strategy",
     metric_column="metric",
-    direction="max",        # "min" wenn kleiner besser ist
+    direction="min",        # "min" wenn kleiner besser ist
     comparison="relative",  # oder "absolute"
-    title=f"Performance Profile Lower Bound (All Models, {GRAPH_TYPE})",
+    title=f"Performance Profile (All Models, {GRAPH_TYPE}, Preprocessed)",
     highlight_best=True,
 )
 
 #plt.show()
 
-ax.figure.savefig(f"benchmarking/plots/all_performance_profile_{GRAPH_TYPE}_lb.png", dpi=300, bbox_inches="tight")
+ax.figure.savefig(f"benchmarking/plots/all_performance_profile_{GRAPH_TYPE}_obj_preprocessed.png", dpi=300, bbox_inches="tight")
 
--- sheets/05_graph_coloring/gurobi_perfomance_profile.py
+++ sheets/05_graph_coloring/gurobi_perfomance_profile.py
@@ -1,7 +1,7 @@
 from models.gurobi.ass_grb import ASSGRB
 from models.gurobi.ass_s_grb import ASS_SGRB
 from models.gurobi.rep_grb import REP_GRB
-
+from models.preprocessor import GCPreprocessor
 
 from _gclib import GCGraphInstance
 from benchmarking import plot_performance_profile as ppp
@@ -10,44 +10,34 @@ import pandas as pd
 import matplotlib
 matplotlib.use("Agg")
 
-GRAPH_TYPE = "barabasi_albert"
+GRAPH_TYPE = "erdos_renyi"
 
 
 instances = []
 strategies = []
 metrics = []
 
-instances_lb = []
-strategies_lb = []
-metrics_lb = []
-
 gc = GCGraphInstance()
 
 
 for i in range(10):
-    graph = gc.graphs[f"{GRAPH_TYPE}_graph{i}"]
+    graph_ = gc.graphs[f"{GRAPH_TYPE}_graph{i}"]
+
+
+    pp = GCPreprocessor(graph_)
+    graph = pp.preprocess()
 
     ng = ASSGRB(graph)
     sol_ng = ng.solve(60)
-    print(sol_ng)
     instances.append(i)
     strategies.append("ASS_GRB")
     metrics.append(sol_ng)
-
-    instances_lb.append(i)
-    strategies_lb.append("ASS_GRB")
-    metrics_lb.append(ng.lb)
-
 
     ms = ASS_SGRB(graph)
     sol_ms = ms.solve(60)
     instances.append(i)
     strategies.append("ASS_S_GRB")
     metrics.append(sol_ms)
-
-    instances_lb.append(i)
-    strategies_lb.append("ASS_S_GRB")
-    metrics_lb.append(ng.lb)
 
 
     ds = REP_GRB(graph)
@@ -56,20 +46,11 @@ for i in range(10):
     strategies.append("REP_GRB")
     metrics.append(sol_ds)
 
-    instances_lb.append(i)
-    strategies_lb.append("REP_GRB")
-    metrics_lb.append(ng.lb)
 
-
-with open(f"grb_{GRAPH_TYPE}_dataset_obj.txt", "w", encoding="utf-8") as file:
+with open(f"datasets/grb_{GRAPH_TYPE}_dataset_obj_preprocessed.txt", "w", encoding="utf-8") as file:
     file.write(f"[{', '.join(map(str, instances))}]\n")
     file.write(f'''["{'", "'.join(strategies)}"]\n''')
     file.write(f"[{', '.join(map(str, metrics))}]\n")
-
-with open(f"grb_{GRAPH_TYPE}_dataset_lb.txt", "w", encoding="utf-8") as file:
-    file.write(f"[{', '.join(map(str, instances_lb))}]\n")
-    file.write(f'''["{'", "'.join(strategies_lb)}"]\n''')
-    file.write(f"[{', '.join(map(str, metrics_lb))}]\n")
 
 
 data = pd.DataFrame({
